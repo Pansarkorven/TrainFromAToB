38a867b74ba1999b7310cc98458c00994491e0ad
Lucas Tidebring <lucas.tidebring@learnet.se>
2025-03-27 10:46:21 +0100

    beginning os inspect code

--- Assets/Script Luctid Gaming07 Sharp sigma bruh cool gaming man 360 noscope ez gamer geometery dash man/InspectCode.cs
+++ Assets/Script Luctid Gaming07 Sharp sigma bruh cool gaming man 360 noscope ez gamer geometery dash man/InspectCode.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Net.NetworkInformation;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 using UnityEngine.UIElements;
 using Cursor = UnityEngine.Cursor;
 
@@ -12,40 +11,31 @@ public class InspectCode : MonoBehaviour
     public GameObject offset;
     private PlayerInput _playerInput;
     GameObject targetObject;
-
     public bool isExamining = false;
-
     public Canvas _canva;
-
     public GameObject tableObject;
-
     private Vector3 lastMousePosition;
+    private Transform examinedObject;
 
-    private Transform examinedObject; // Store the currently examined object
-
-
-    //List of position and rotation of the interactble objects 
     private Dictionary<Transform, Vector3> originalPositions = new Dictionary<Transform, Vector3>();
     private Dictionary<Transform, Quaternion> originalRotations = new Dictionary<Transform, Quaternion>();
 
-
+    private CinemachineBrain cinemachineBrain;
 
     void Start()
     {
         _canva.enabled = false;
         targetObject = GameObject.Find("Player");
         _playerInput = targetObject.GetComponent<PlayerInput>();
+cinemachineBrain = GameObject.Find("Camera").GetComponent<CinemachineBrain>();
     }
 
     void Update()
     {
-        // it performs a raycast from the camera to the mouse position and checks if it hits an object tagged as "Object."
-        // If it does, it toggles the examination state and stores the examined object's original position and rotation.
-
         if (Input.GetKeyDown(KeyCode.E))
         {
-
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Camera mainCamera = cinemachineBrain.OutputCamera; // Get Cinemachine-controlled camera
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             ToggleExamination();
@@ -54,65 +44,46 @@ public class InspectCode : MonoBehaviour
             {
                 if (hit.collider.CompareTag("Object"))
                 {
-
-
-                    // Store the currently examined object and its original position and rotation
                     if (isExamining)
                     {
-
                         examinedObject = hit.transform;
                         originalPositions[examinedObject] = examinedObject.position;
                         originalRotations[examinedObject] = examinedObject.rotation;
                     }
                 }
             }
-
-
         }
-
-
-        //It then checks if the player is close to an interactable object using the CheckUserClose() method.
-        //If the player is close, it calls either Examine() or NonExamine() and enables or disables the canvas component accordingly.
 
         if (CheckUserClose())
         {
-
-
             if (isExamining)
             {
                 _canva.enabled = false;
-                Examine(); StartExamination();
+                Examine();
+                StartExamination();
             }
             else
             {
                 _canva.enabled = true;
-                NonExamine(); StopExamination();
+                NonExamine();
+                StopExamination();
             }
         }
         else _canva.enabled = false;
-
     }
 
     public void ToggleExamination()
     {
         isExamining = !isExamining;
-
     }
-
-    // This method is called when the player starts examining an object. It locks the cursor,
-    // makes it visible, and disables the PlayerInput component to prevent player movement during examination.
 
     void StartExamination()
     {
-
         lastMousePosition = Input.mousePosition;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         _playerInput.enabled = false;
     }
-
-    //This method is called when the player stops examining an object. It locks the cursor again,
-    //hides it, and re-enables the PlayerInput component to allow player movement.
 
     void StopExamination()
     {
@@ -121,16 +92,11 @@ public class InspectCode : MonoBehaviour
         _playerInput.enabled = true;
     }
 
-
-    // This method is called when the player is examining an object.
-    // It moves the examined object towards the offset object and allows the player to rotate it based on mouse movement.
-
     void Examine()
     {
         if (examinedObject != null)
         {
             examinedObject.position = Vector3.Lerp(examinedObject.position, offset.transform.position, 0.2f);
-
             Vector3 deltaMouse = Input.mousePosition - lastMousePosition;
             float rotationSpeed = 1.0f;
             examinedObject.Rotate(deltaMouse.x * rotationSpeed * Vector3.up, Space.World);
@@ -139,14 +105,10 @@ public class InspectCode : MonoBehaviour
         }
     }
 
-    //This method is called when the player is not examining an object.
-    //It resets the position and rotation of the examined object to its original values stored in the dictionaries.
-
     void NonExamine()
     {
         if (examinedObject != null)
         {
-            // Reset the position and rotation of the examined object to its original values
             if (originalPositions.ContainsKey(examinedObject))
             {
                 examinedObject.position = Vector3.Lerp(examinedObject.position, originalPositions[examinedObject], 0.2f);
@@ -158,17 +120,9 @@ public class InspectCode : MonoBehaviour
         }
     }
 
-
-    // This method calculates the distance between the player(targetObject) and 
-    // an object called tableObject.If the distance is less than 2 units, it returns true, indicating that the player is close to the object.
     bool CheckUserClose()
     {
-        // Calculate the distance between the two GameObjects
         float distance = Vector3.Distance(targetObject.transform.position, tableObject.transform.position);
-
-        // Check if they are close based on the threshold
         return (distance < 2);
-
     }
-
 }

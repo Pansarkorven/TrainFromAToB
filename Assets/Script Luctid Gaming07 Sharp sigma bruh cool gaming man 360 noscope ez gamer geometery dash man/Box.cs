c7b526a0dd9aec4c7fc30bbdc78ccbf420370d7d
Lucas Tidebring <lucas.tidebring@learnet.se>
2025-05-15 09:04:17 +0200

    Gjorde så Pressure Pad, Box och Door funka

---
+++ Assets/Script Luctid Gaming07 Sharp sigma bruh cool gaming man 360 noscope ez gamer geometery dash man/Box.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public Transform holdPoint; // Where the box will be held (e.g., in front of the camera)
+    public Canvas pickupCanvas; // UI canvas showing "Press E to pick up"
+    public float pickupRange = 3f;
+    public string cameraTag = "MainCamera"; // Tag to find the player's camera
+
+    private bool isPickedUp = false;
+    private Rigidbody rb;
+    private Transform cameraTransform;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (pickupCanvas != null)
+            pickupCanvas.enabled = false;
+
+        // Find the camera by tag
+        GameObject camObj = GameObject.FindGameObjectWithTag(cameraTag);
+        if (camObj != null)
+        {
+            cameraTransform = camObj.transform;
+        }
+    }
+
+    void Update()
+    {
+        if (cameraTransform == null) return;
+
+        float distance = Vector3.Distance(transform.position, cameraTransform.position);
+
+        if (!isPickedUp)
+        {
+            if (distance <= pickupRange)
+            {
+                if (pickupCanvas != null)
+                    pickupCanvas.enabled = true;
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    PickUp();
+                }
+            }
+            else
+            {
+                if (pickupCanvas != null)
+                    pickupCanvas.enabled = false;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Drop();
+            }
+        }
+    }
+
+    void PickUp()
+    {
+        isPickedUp = true;
+        rb.isKinematic = true;
+        transform.SetParent(holdPoint);
+        transform.localPosition = Vector3.zero;
+
+        if (pickupCanvas != null)
+            pickupCanvas.enabled = false;
+    }
+
+    void Drop()
+    {
+        isPickedUp = false;
+        rb.isKinematic = false;
+        transform.SetParent(null);
+    }
+}
--- Assets/Scripts_Fabbe/PressurePlate.cs
+++ Assets/Scripts_Fabbe/PressurePlate.cs
@@ -1,89 +1,55 @@
 using UnityEngine;
 
-public class PressureButton : MonoBehaviour
+public class PressurePlate : MonoBehaviour
 {
-    public string targetTag = "PickupItem";
-    public bool useTrigger = true; // Toggle between Trigger or Collision
-    public GameObject buttonVisual;
-    public Vector3 pressedOffset = new Vector3(0, -0.05f, 0);
-    public float moveSpeed = 3f;
 
-    private int pressCount = 0;
-    private Vector3 originalPosition;
+    public GameObject door;
 
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        if (buttonVisual == null) buttonVisual = this.gameObject;
-        originalPosition = buttonVisual.transform.localPosition;
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (!useTrigger) return;
-        if (other.CompareTag(targetTag))
+        if (collision.gameObject.CompareTag("Box"))
         {
-            pressCount++;
-            if (pressCount == 1)
-                Press();
+            Debug.Log("Box on pad");
+            Debug.Log("Door is Open");
+            OpenDoor();
+
         }
     }
 
-    void OnTriggerExit(Collider other)
+
+    private void OnCollisionExit(Collision collision)
     {
-        if (!useTrigger) return;
-        if (other.CompareTag(targetTag))
+        if (collision.gameObject.CompareTag("Box"))
         {
-            pressCount--;
-            if (pressCount == 0)
-                Release();
+            Debug.Log("Box left pad");
+            Debug.Log("Door is Closed");
+            CloseDoor();
+
+
+
+        }
+
+    }
+
+
+    private void OpenDoor()
+    {
+        if (door != null)
+        {
+            door.SetActive(false);  
+            Debug.Log("Door opens");
         }
     }
 
-    void OnCollisionEnter(Collision collision)
+    private void CloseDoor()
     {
-        if (useTrigger) return;
-        if (collision.gameObject.CompareTag(targetTag))
+        if (door != null)
         {
-            pressCount++;
-            if (pressCount == 1)
-                Press();
+            door.SetActive(true);   
+            Debug.Log("Door is locked");
         }
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        if (useTrigger) return;
-        if (collision.gameObject.CompareTag(targetTag))
-        {
-            pressCount--;
-            if (pressCount == 0)
-                Release();
-        }
-    }
-
-    void Press()
-    {
-        StopAllCoroutines();
-        StartCoroutine(MoveButton(originalPosition + pressedOffset));
-        Debug.Log("Button Pressed!");
-        // Add your logic here (e.g. open a door)
-    }
-
-    void Release()
-    {
-        StopAllCoroutines();
-        StartCoroutine(MoveButton(originalPosition));
-        Debug.Log("Button Released!");
-        // Add your logic here (e.g. close a door)
-    }
-
-    System.Collections.IEnumerator MoveButton(Vector3 targetPos)
-    {
-        while (Vector3.Distance(buttonVisual.transform.localPosition, targetPos) > 0.001f)
-        {
-            buttonVisual.transform.localPosition = Vector3.Lerp(buttonVisual.transform.localPosition, targetPos, Time.deltaTime * moveSpeed);
-            yield return null;
-        }
-        buttonVisual.transform.localPosition = targetPos;
     }
 }
+
+
+
